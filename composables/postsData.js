1b6a1e39f29a93c5cf550bb455dd3902e6e6dd65
Rofik Bhi <alirofikr@gmail.com>
2023-12-20 14:40:55 +0530

    composable with userlazyFetch functio

--- composables/postsData.js
+++ composables/postsData.js
@@ -1,28 +1,44 @@
 import { ref } from 'vue'
 import axios from 'axios';
 
+const route = useRoute()
 
 export default function useShowPosts() {
     const url = 'http://127.0.0.1:8000/'
-    // const slug = ref([])
-    const posts = ref([])
-    const error = ref('this is error')
+    const { pending, data: posts, error } = useLazyFetch(url);
 
+    const posts_data = ref();
 
     const getPosts = async () => {
 
-        posts.value = []
-        // error.value = []
-
-        try {
-            const res = await axios(url)
-            console.log('response is her', res.data);
-            posts.value = res.data
-        } catch (err) {
-            console.log('error block is here.', err.value);
+        if (pending) {
+            console.log('pending ... PostsData', pending);
+        } else {
+            posts = posts_data.value
         }
 
+        // try {
+        //     posts = posts
+        //     console.log('response is her', posts);
+        // } catch (err) {
+        //     console.log('error block is here.', err.error);
+        // }
+
     }
+    // const getPosts = async () => {
+
+    //     posts.value = []
+    //     // error.value = []
+
+    //     try {
+    //         const res = await axios(url)
+    //         console.log('response is her', res.data);
+    //         posts.value = res.data
+    //     } catch (err) {
+    //         console.log('error block is here.', err.value);
+    //     }
+
+    // }
 
     return {
         posts,
