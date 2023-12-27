631fc9d0f08c489cd125cf1f2fae53c9378329cd
Rofik Bhi <alirofikr@gmail.com>
2023-12-27 09:20:38 +0530

    added and deleted somethng

--- LTS/LTS/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('api_blog.urls', namespace='api_blogs')),
-    path('', include('api_courses.urls', namespace='api_courses')),
-    # path('accounts/', include('accounts.urls', namespace='accounts')),
-]
--- LTS/api_courses/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiCoursesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api_courses'
