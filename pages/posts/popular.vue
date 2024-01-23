d2f0d40b11a1b99e367b80ea8485ff29145f8b2d
Rofik Bhi <alirofikr@gmail.com>
2024-01-23 11:03:02 +0530

    this is done

--- pages/posts/popular.vue
+++ pages/posts/popular.vue
@@ -1,11 +1,55 @@
+<script setup>
+
+const data = [
+    {
+        'id': 1,
+        'title': 'this is first title'
+    },
+    {
+        'id': 2,
+        'title': 'this is second title'
+    },
+    {
+        'id': 3,
+        'title': 'this is third title'
+    },
+]
+
+</script>
 <template>
-    <div>
-        <h1>Popular one here.</h1>
+    <div class='container'>
+        <div class="listing-data">
+            <div v-for="item in data" :key="item.id">
+                <NuxtLink :to="{ name: 'posts-popular-id', params: { id: item.id } }">
+                    <h2>Title - {{ item.title }}</h2>
+                </NuxtLink>
+            </div>
+        </div>
+        <div class="single-data">
+            <h2>NuxtPage</h2>
+            <NuxtPage />
+        </div>
     </div>
 </template>
 
-<script setup>
 
-</script>
+<style scoped>
+.container {
+    display: flex;
+    border: 2px solid rgb(0, 38, 255);
+    width: 98%;
+    gap: 10px;
+    text-align: center;
+    justify-content: center;
+}
 
-<style scoped></style>
+.listing-data {
+    border: 2px solid rgb(128, 0, 0);
+    width: 25%;
+}
+
+.single-data {
+    border: 2px solid rgb(255, 166, 0);
+    width: 70%;
+}
+</style>
---
+++ pages/posts/popular/[id].vue
@@ -0,0 +1,16 @@
+<script setup>
+const data = {
+    'id': 1,
+    'title': 'this is first title'
+}
+</script>
+
+<template>
+    <div>
+        <h1>{{ data.id }}</h1>
+        <h2>{{ data.title }}</h2>
+    </div>
+</template>
+
+
+<style scoped></style>
